7cd74b35373739665ef9dbe81dde99ba828ba918
Wenxin Wang <i@wenxinwang.me>
2019-03-23 12:46:15 +0800

    termux wake lock

--- termux-tasker/org-imap-replace.sh
+++ termux-tasker/org-imap-replace.sh
@@ -1,4 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 export LD_PRELOAD=${PREFIX}/lib/libtermux-exec.so
+fin () {
+  termux-wake-unlock
+  echo bye
+}
+trap fin EXIT
+termux-wake-lock
 REPLACE=1 /data/data/com.termux/files/usr/bin/bash ~/paul/org-imap.sh ~/storage/shared/orgzly/Inbox.org
